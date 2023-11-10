a4e30c6cb666c22d02ef2413dc5dbacf6f0c6dff
nizam <nizudin120@gmail.com>
2023-11-10 15:42:53 +0530

    about-component-responsive1

--- components/Services.js
+++ components/Services.js
@@ -11,7 +11,7 @@ function Services() {
                 <p>Google is top search engine thats been most used by the internet surfers to quickly search and find what they are looking for and this being the reason it becomes very crucial for businesses being available to the right audiences and for the searches on the Google search engine or the Bing search engine. The beautiful part is Google allows the businesses to make their presence by taking part of the auction using the Google ads platform.Google is top search engine thats been most used by the internet surfers to quickly search and find what they are looking for and this being the reason it becomes very crucial for businesses being available to the right audiences and for the searches on the Google search engine or the Bing search engine. The beautiful part is Google allows the businesses to make their presence by taking part of the auction using the Google ads platform.</p>
 
             </div>
-            <div className="scroll d-flex flex-column">
+            <div className="scroll-service d-flex flex-column">
                 <div className="container">
                     <div className="d-flex">
 
